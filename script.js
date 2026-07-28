a3d423b6131208969337f7587b89471eddbb271b
sumannooh <aiartsuman@gmail.com>
2026-07-28 19:56:35 +0530

    Add login form submission alert

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const button = document.querySelector(".hero button");
 button.addEventListener("click", () => {
     alert("Welcome to OpenMCN!");
 });
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+  loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Login feature coming soon!");
+  });
+}
