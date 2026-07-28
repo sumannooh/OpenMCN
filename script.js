2b52845a8343ee912ecc98ceab3d0d71e31a5109
sumannooh <aiartsuman@gmail.com>
2026-07-28 21:26:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,4 @@ const button = document.querySelector(".hero button");
 
 button.addEventListener("click", () => {
     alert("Welcome to OpenMCN!");
-});
-const loginForm = document.getElementById("loginForm");
 
-if (loginForm) {
-  loginForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Login feature coming soon!");
-  });
-}
-const signupForm = document.getElementById("signupForm");
-
-if (signupForm) {
-    signupForm.addEventListener("submit", function(e) {
-        e.preventDefault();
-
-        alert("Account Created Successfully!");
-
-        window.location.href = "login.html";
-    });
-}
